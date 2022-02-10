47b4b592ef0132ed17442175b9e8aa0dafe000dd
abditake <abditake@hotmail.com>
2022-02-09 22:27:51 -0800

    update to app file

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 `use strict`;
-console.log('hey world');
+
 
 let userName = prompt('What is your name');
-      alert(`welcome ${userName} , it's very nice to meet you.`);
+alert(`welcome ${userName} , it's very nice to meet you.`);
 
-  //first question
+let score = 0
+
+//first question
 let answerOne = prompt('Do I like anime?').toLowerCase();
 if (answerOne === 'yes' || answerOne === 'y') {
   alert('You are Correct!!');
-  console.log('you are correct!')
+  score++
 } else if (answerOne === 'no' || answerOne === 'n') {
 
   alert('you are incorrect!!');
-  console.log('you are incorrect.');
 }
 else {
   alert('Input not valid');
@@ -23,10 +24,9 @@ let answerTwo = prompt('Do I like to play games?').toLowerCase();
 console.log(answerTwo);
 if (answerTwo === 'yes' || answerTwo === 'y') {
   alert('You are Correct!!');
-  console.log('you are correct!')
+  score++
 } else if (answerTwo === 'no' || answerTwo === 'n') {
   alert('you are incorrect!! ');
-  console.log('you are incorrect.');
 }
 else {
   alert('Input not valid');
@@ -35,10 +35,9 @@ else {
 let answerThree = prompt('Do I like fishing?').toLowerCase();
 if (answerThree === 'yes' || answerThree === 'y') {
   alert('You are Correct!!');
-  console.log('you are correct!');
+  score++
 } else if (answerThree === 'no' || answerThree === 'n') {
   alert('you are incorrect!! ');
-  console.log('you are incorrect');
 }
 else {
   alert('Input not valid');
@@ -48,7 +47,7 @@ let answerFour = prompt('Do I like Pizza?').toLowerCase();
 console.log(answerFour);
 if (answerFour === 'yes' || answerFour === 'y') {
   alert('You are Correct!!');
-  console.log('you are correct!');
+  score++
 } else if (answerFour === 'no' || answerFour === 'n') {
   alert('you are incorrect!! ');
   console.log('you are incorrect.');
@@ -57,4 +56,53 @@ else {
   alert('Input not valid');
 }
 
-alert(`thank you ${userName}, for answering my questions!!. I hope you have a nice day`);
+
+//6th questions
+const myNumber = 2
+for (let counter = 0; counter < 4; counter++) {
+  let input = prompt('Guess a number 1-10.');
+  if (input == myNumber) {
+    alert('congrats you got it right!!!');
+    score++;
+    break;
+  }
+  else if (input > myNumber) {
+    alert('too high');
+  }
+  else if (input < myNumber) {
+    alert('too low');
+  }
+  else {
+    alert('Input invalid');
+  }
+
+}
+
+
+//7th questions
+let snacks = ['chips', 'cookies', 'fruit', 'vegetables'];
+let rightAnswer = false // false becuase so far they haven't got it right
+for (let tries = 0; tries < 6; tries++) {
+  let  input = prompt('Guess my favorite snack.');
+  // this loop is to access the values of the array.to check tha 
+  for (let i = 0; i < snacks.length; i++) { 
+    // this checks the input against the array 
+    if (input === snacks[i]) {
+      rightAnswer = true //to get out of the outer for loop
+      score++;
+      console.log(snacks[i]);
+      alert('You are correct!');
+      break;//gets you out of inner loop
+    }
+  }
+  // this loop is to get out of the outer for loop
+  if (rightAnswer === true) {
+    break;
+  }
+  alert('You are wrong');
+}
+alert(`The correct answers are ${snacks}`)
+alert(`You answered ${score} out of 7 questions right`)
+
+
+
